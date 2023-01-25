491ec367f8e7a4ce4325691406b116d0b0203eb8
Simone Capuano <scapuano19@gmail.com>
2023-01-25 12:39:44 +0100

    home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
+import Head from 'next/head'
+import Layout from '@/components/layout/layout'
 import '@/styles/globals.css'
+import { NotificationContextProvider } from '@/store/notification-context'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NotificationContextProvider>
+      <Layout>
+          <Head>
+            <title>Next Events</title>
+            <meta name="description" content="find a lot of great events" />
+            <meta name="viewport" content="width=device-width, initial-scale=0.1" />
+          </Head>
+          <Component {...pageProps} />
+        </Layout>
+      </NotificationContextProvider>
+  )
 }
