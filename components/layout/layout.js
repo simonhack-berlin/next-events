2d6a20c5e31976bd0435a0b6ef5d2a7f798b0312
Simone Capuano <scapuano19@gmail.com>
2023-01-25 12:27:18 +0100

    layout and main-header components created

---
+++ components/layout/layout.js
@@ -0,0 +1,26 @@
+import { Fragment, useContext } from "react";
+import NotificationContext from "../../store/notification-context";
+import Notification from "../notification/notification";
+import MainHeader from "./main-header";
+
+function Layout(props) {
+    const notificationCtx = useContext(NotificationContext);
+
+    const activeNotification = notificationCtx.notification
+
+    return (
+        <Fragment>
+            <MainHeader />
+            <main>
+                {props.children}
+            </main>
+            {activeNotification && <Notification 
+                    title={activeNotification.title} 
+                    message={activeNotification.message}
+                    status={activeNotification.status}
+                />}
+        </Fragment>
+    )
+}
+
+export default Layout;
